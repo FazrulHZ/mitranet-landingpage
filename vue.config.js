cda36f4acf9191cbb9488026d440115e97a91850
fazrulhz <ardin260@gmail.com>
2023-05-05 23:28:23 +0800

    Perbaikan vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pwa: {
+    name: 'Mitranusa.id',
+    themeColor: '#212121',
+    msTileColor: '#000000',
+    display: 'standalone',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-icon-152x152.png',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
+  },
 }
