943d088b3ef4b2cd8286fc68b8846a02f2d62892
fazrulhz <ardin260@gmail.com>
2023-10-12 11:31:24 +0800

    Perubahan config route

--- src/router/index.js
+++ src/router/index.js
@@ -68,7 +68,7 @@ const routes = [{
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/app/web',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: "/app/web" ,
   "transpileDependencies": [
     "vuetify"
   ],
